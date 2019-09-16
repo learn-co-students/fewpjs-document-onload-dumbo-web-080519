0f14fbaafd19c5b6ce3111de61accf963ee366c4
wukrit <sukritwalia@gmail.com>
2019-09-16 14:19:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  let pTag = document.querySelector('#text');
+  pTag.textContent = "This is really cool!"
+});
