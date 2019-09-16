6b5edb8b1b824cfca9dc2d264412cd78bd98332b
mrwerner392 <mrwerner392@gmail.com>
2019-09-16 12:44:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  let par = document.querySelector("#text");
+  changeParagraphText(par);
+});
+
+function changeParagraphText(par) {
+  par.textContent = "This is really cool!"
+}
